4a51f78c2a9024ec79272694d924d90db7aa2951
root <root@localhost.localdomain>
2020-03-07 22:41:27 +0200

    function for checking is the content of the file is correct

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -1,6 +1,5 @@
 // package simulator;
 import java.io.*;
-import java.net.URLConnection;
 import java.nio.file.Path;
 import java.nio.file.Files;
 import java.nio.file.FileSystems;
@@ -15,13 +14,16 @@ class MyException extends Exception {
 }
 
 class Exceptionhanddling{
+    //checking if the file provided is a .txt file and that there is a file provided as input, also check if the file exist 
     public void checkInput(String args){
         try {
             File file = new File(args);
+            //checking if the file exist
             if (!file.exists()){
                 throw new MyException("please provide a text file as args");
             }
             Path path = FileSystems.getDefault().getPath(".", args);
+            //checking the type of the file to make sure that it is a .txt file
             String mimeType = Files.probeContentType(path);
             if (!mimeType.equals("text/plain")){
                 throw new MyException("file not text");
@@ -35,6 +37,28 @@ class Exceptionhanddling{
             System.exit(0);
         }        
     }
+
+    //checking if the input file is formated correctly
+    public void checkFile(String args){
+        int i = 0;
+        String input;
+
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(args));
+            //checking if the input can be converted in an integer (if it is a number)
+            Integer.parseInt(br.readLine());
+            //checking if there are five element after the first line of input
+            while ((input = br.readLine()) != null){
+                if (input.split(" ").length != 5){
+                    throw new MyException("The file must have 5 elements in this form: TYPE LONGITUDE HEIGHT LATITUDE HEIGHT");
+                }
+            }
+        }catch (NumberFormatException e) {
+            System.out.println("The first line should be a interger");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
 
 public class Simulator{
@@ -42,16 +66,12 @@ public class Simulator{
         try{
             Exceptionhanddling check = new Exceptionhanddling();
             check.checkInput(args[0]);
+            check.checkFile(args[0]);
         }catch(Exception e){
             if (e.getMessage().equals("Index 0 out of bounds for length 0")){
                 System.out.println("please provide simulation text file as args");
                 System.exit(0);
             }
-        }
-        File file = new File(args[0]); 
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        // String input = br.readLine();    
-        // String[] input = args[0].split(" ", 5);
-        // System.out.println(input);
+        } 
     }
 }
